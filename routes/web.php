fbfb6538f9797f851b84e0f98f0146159837e7e4
hiphop200199 <hiphop200199@gmail.com>
2025-02-13 01:03:22 +0800

    ;

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use App\Http\Controllers\SongController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
+
 Route::get("getSongs", [SongController::class,'getSongs']);
 Route::get("getSong", [SongController::class,'getSong']);
